3c925611c84a933ef3994bf924f78a9d76d73f5c
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-06-29 21:23:52 +0000

    trouble shooting
    struggle bus to to get pow function to work  correctly.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 //This program will allow you to calculate interrest earned within the year on a savings account.
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 
 int main ()
 {
-  
+  double principal, Rate, T, Amount;
+  cout<< "What is the principal amount?" << endl;
+  cin>> principal;
+  cout<< "What is the interest rate?" << endl;
+  cin>> Rate;
+  cout<< "How often is the interest rate compounded?" << endl;
+  cin>> T;
+  Amount = principal *( 1 +Rate / T);
+  return 0;
 }

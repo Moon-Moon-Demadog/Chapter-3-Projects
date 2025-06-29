64dd826b0d937d940f1e8c86bf1eead66046d9c4
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-06-29 20:11:18 +0000

    finally got it "true" randomized.

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -4,21 +4,21 @@
 #include <cmath>
 #include <random>
 using namespace std;
-
 int main ()
 {
+  srand(time(NULL));
   long number1, number2, Answer, Sum;
   const int MIN_VALUE = 0;
   const int MAX_VALUE = 350;
-  unsigned seed = time (0);
   number1 = (rand() % (MAX_VALUE-MIN_VALUE));
+  number2 = (rand() % (MAX_VALUE-MIN_VALUE));
   Sum = number1 + number2;
   cout<< "Lets practice some basic addition." << endl;
   cout << number1 << " + " << number2 << endl;
-  cout << "What is the sum of these two numbers?" << endl;
+  cout <<":   " << endl;
   cin>> Answer;
   if (Answer != Sum)
-    cout<< Answer << " is incorrect" << Sum << " is the correct answer." << endl;
+    cout<< Answer << " is incorrect " << Sum << " is the correct answer." << endl;
   else
     cout << "That is correct!" << endl;
   return 0;

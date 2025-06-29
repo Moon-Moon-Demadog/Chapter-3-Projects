d0a73c85b8408bb62226659b32aa61e3594fe12b
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-06-29 18:21:29 +0000

    resolved errors
    Wasn't able to do user input for the # of cookies, was messing around with the cin expressions, cout and even variables. finally changed the wording in line 10 and ommited the word 'Type' and it did the trick. Program is running as intended.

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,23 +1,24 @@
 //This program will adjust ingredients based off of how many cookies you want to bake.
 #include <iostream>
 using namespace std;
-int man ()
+int main ()
 {
   double flour, sugar, butter, NewFlour, NewSugar, NewButter, NewCookies;
   int OGcookies, cookies;
-  flour = 2.5;
+
+  cout<< "Lets make a batch of cookies!! A standard batch of cookies yields aproximately 48 cookies." << endl;
+  cout<< " number of cookies you'd like per batch and hit enter : ";
+  cin>> cookies ;
+  
+  OGcookies = 48;
+  NewCookies = cookies / OGcookies;
+  flour = 2.75;
   sugar = 1.5;
   butter = 1.0;
   NewFlour = flour * NewCookies;
   NewSugar = sugar * NewCookies;
   NewButter = butter * NewCookies;
-  NewCookies = cookies % OGcookies;
-  OGcookies = 48;
-
-  cout<< "Lets make a batch of cookies!! A standard batch of cookies yields aproximately 48 cookies." << endl;
-  cout<< "With the standard reciepe you'll need " << flour << " cups of flour, " << sugar << " cups of sugar and " << butter << " cups of butter." << endl;
-  cout<< "Type the number of cookies you'd like per batch and hit enter." << endl;
-  cin>> cookies;
+  
   cout<< "Perfect, you'll need " << NewFlour << " cups of flour, " << NewSugar << " cups of sugar and " << NewButter << " cups of butter to bake your cookies!" << endl;
   return 0;
 }
